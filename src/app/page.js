60a2732d2c2714d225d0a56cb0c640c441ee43bc
Raphael Frimpong <raphaelfrimpong176@gmail.com>
2024-11-23 15:50:49 -0600

    fix bugs

--- src/app/page.js
+++ src/app/page.js
@@ -25,16 +25,16 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="/submit-idea"
-            target="_blank"
+            href="/login"
+            target="_self"
             rel="noopener noreferrer"
           >
             Sign in
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
+            href="/signup"
+            target="_self"
             rel="noopener noreferrer"
           >
             Sign up
@@ -72,21 +72,6 @@ export default function Home() {
           />
           Examples
         </a> */}
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          View Ideas
-        </a>
       </footer>
     </div>
   );
