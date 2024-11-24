ff2354828c1f4a2d0575151c794f155a3796da40
David Nintang <nintang48@gmail.com>
2024-11-23 20:42:50 -0600

    integrated navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -18,7 +18,7 @@ export default function Navbar({currentTab}) {
     const navigation = [
         {name: "Submit Idea", href: "/submit-idea", key: "submit-idea"},
         {name: "View Ideas", href: "/view-ideas", key: "view-ideas"},
-        // {name: "Projects", href: "#", key: "projects"},
+        {name: "Recommend", href: "/recommend", key: "recommend"},
         // {name: "Calendar", href: "#", key: "calendar"},
     ];
 
@@ -72,14 +72,7 @@ export default function Navbar({currentTab}) {
                         </div>
                     </div>
                     <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                        <button
-                            type="button"
-                            className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                        >
-                            <span className="absolute -inset-1.5" />
-                            <span className="sr-only">View notifications</span>
-                            <BellIcon aria-hidden="true" className="size-6" />
-                        </button>
+                        
 
                         {/* Profile dropdown */}
                         <Menu as="div" className="relative ml-3">
@@ -91,7 +84,7 @@ export default function Navbar({currentTab}) {
                                     </span>
                                     <img
                                         alt=""
-                                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                        src="../../avatar.webp"
                                         className="size-8 rounded-full"
                                     />
                                 </MenuButton>
@@ -118,7 +111,7 @@ export default function Navbar({currentTab}) {
                                 </MenuItem>
                                 <MenuItem>
                                     <a
-                                        href="#"
+                                        href="/"
                                         className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
                                     >
                                         Sign out
